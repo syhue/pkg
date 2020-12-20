9f055444ce84c04a6fdd2672d864d23db4cc5214
syhue <sook.yan.hsy@gmail.com>
2020-12-20 10:56:17 +0800

    update file

--- nav.php
+++ nav.php
@@ -62,16 +62,21 @@
 
                 $('.category1').click(function() {
                     $('.content1').toggle();
-
+                    $('.content2').hide();
+                    $('.content3').hide();
                 });
 
                 $('.category2').click(function() {
                     $('.content2').toggle();
+                    $('.content1').hide();
+                    $('.content3').hide();
 
                 });
 
                 $('.category3').click(function() {
                     $('.content3').toggle();
+                    $('.content1').hide();
+                    $('.content2').hide();
 
                 });
               }
